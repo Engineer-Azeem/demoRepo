4d6eca0b81fac9c6fd14d3a0219d6b80a0d005cb
Azeem <muhammadazeem1321@gmail.com>
2022-08-31 19:19:39 +0500

    Modified HelloWord.java for scenario 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); 
+        System.out.println("Scenario 1: Build Hello World using Jenkins"); 
     }
 }
